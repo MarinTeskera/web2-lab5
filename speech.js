0f97dafa7e64bb1398886d9f1e94c5032c158756
Marin Teskera <marin.teskera@infinum.com>
2023-12-16 14:10:54 +0100

    speech to text example

---
+++ speech.js
@@ -0,0 +1,29 @@
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechGrammarList =
+  window.SpeechGrammarList || window.webkitSpeechGrammarList;
+const SpeechRecognitionEvent =
+  window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+const recognition = new SpeechRecognition();
+const speechRecognitionList = new SpeechGrammarList();
+recognition.grammars = speechRecognitionList;
+recognition.continuous = false;
+recognition.lang = "en-US";
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+const diagnostic = document.querySelector(".output");
+const record = document.getElementById("record");
+
+const bg = document.querySelector("html");
+
+record.onclick = () => {
+  recognition.start();
+  console.log("Ready to receive a color command.");
+};
+
+recognition.onresult = (event) => {
+  const color = event.results[0][0].transcript;
+  diagnostic.textContent = `Result received: ${color}`;
+  bg.style.backgroundColor = color;
+};
