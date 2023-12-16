90a7c168fbaa24a5d0f9f77410c8505aaac7a9a9
Marin Teskera <marin.teskera@infinum.com>
2023-12-16 11:55:05 +0100

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const httpPort = 80;
+
+const app = express();
+app.use(express.json()); // za VER06
+
+app.use((req, res, next) => {
+  console.log(new Date().toLocaleString() + " " + req.url);
+  next();
+});
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", VERSION, "index.html"));
+});
+
+app.listen(httpPort, function () {
+  console.log(`HTTP listening on port: ${httpPort}`);
+});
